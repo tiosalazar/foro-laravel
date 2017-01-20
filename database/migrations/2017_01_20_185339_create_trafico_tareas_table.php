f7d6591a83d0f7cd6edc86cf684cb4922ee52ea6
Alvaro Borrero <aborrero@himalayada.com>
2017-01-20 14:32:47 -0500

    migrate trafico_tareas

--- database/migrations/2017_01_20_185339_create_trafico_tareas_table.php
+++ database/migrations/2017_01_20_185339_create_trafico_tareas_table.php
@@ -13,7 +13,22 @@ class CreateTraficoTareasTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('trafico_tareas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre_tarea',45);
+            $table->dateTime('fecha_entrega_estimada')->nullable();
+            $table->string('descripcion',255);
+            $table->string('enlaces_externos',150)->nullable();
+            $table->decimal('tiempo_estimado',5,3)->nullable();
+            $table->decimal('tiempo_real',5,3)->nullable();
+            $table->dateTime('fecha_entrega_cliente');
+            $table->integer('estado_id')->nullable();
+            $table->integer('areas_id')->nullable();
+            $table->integer('usuarios_id')->nullable();
+            $table->integer('ots_id')->nullable();
+            $table->integer('roles_id')->unsigned();
+            $table->timestamps();
+        });
     }
 
     /**
