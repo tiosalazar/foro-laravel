d3151e7f2302016777b87c3eef4eb654d3ff5025
Alvaro Borrero <aborrero@himalayada.com>
2017-02-07 18:02:45 -0500

    cambio de propiedades tarea, crear controlado Tarea y inserccion de tarea en BD

--- resources/assets/js/components/tareas/crear_tarea.vue
+++ resources/assets/js/components/tareas/crear_tarea.vue
@@ -64,34 +64,34 @@
 				<select_area  :refresha="refresh"></select_area>
 			</div>
 			<label>Fecha entrega cliente</label>
-			<div class="input-group date" :class="{ 'has-error': !fecha_entrega_cliente }" >
+			<div class="input-group date" >
 				<div class="input-group-addon">
 					<i class="fa fa-calendar"></i>
 				</div>
 				<datepicker language="es"  id="fecha_entrega_cliente" required="required" placeholder="Fecha fin" v-model="fecha_entrega_cliente" class="form-control"  name="fecha_entrega_cliente" format="dd-MM-yyyy"></datepicker>
 			</div>
-			<div style="padding:2px 0px;"  :class="{ 'has-error': !fecha_entrega_cliente }" v-show="!fecha_entrega_cliente">
-				<span  class="help-block">El campo Fecha Fin es obligatorio</span>
+			<div class="form-group required" v-bind:class="[errors_return.nombre_tarea,{ 'has-error': errors.has('nombre_tarea') }]">
+				<label for="nombre_tarea">Nombre de la Solicitud <sup>*</sup></label>
+				<input type="text" class="form-control"  id="nombre_tarea" v-model="tarea.nombre_tarea" name="nombre_tarea" placeholder="Solicitud" v-validate data-vv-rules="required|alpha_num|min:4" required="required">
+				<span  class="help-block" v-show="errors.has('nombre_tarea')">{{ errors.first('nombre_tarea') }}</span>
 			</div>
-			<div class="form-group required">
-				<label for="nombre_solicitud">Nombre de la Solicitud <sup>*</sup></label>
-				<input type="text" class="form-control"  id="nombre_solicitud" v-model="tarea.nombre_tarea" name="nombre_tarea" placeholder="Solicitud" required="required">
-			</div>
-			<div class="form-group required">
+			<div class="form-group required" v-bind:class="[errors_return.descripcion,{ 'has-error': errors.has('descripcion') }]">
 				<label for="descripcion">Descripción <sup>*</sup></label>
-				<textarea class="form-control" rows="3"  name="descripcion"  id="descripcion" v-model="tarea.descripcion" placeholder="Descripción" required="required"></textarea>
+				<textarea class="form-control" rows="3"  name="descripcion"  id="descripcion" v-model="tarea.descripcion" placeholder="Descripción" required="required" v-validate data-vv-rules="required|min:4"></textarea>
+				<span  class="help-block" v-show="errors.has('descripcion')">{{ errors.first('descripcion') }}</span>
 			</div>
-			<div class="form-group">
+			<div class="form-group" v-bind:class="[errors_return.enlaces_externos,{ 'has-error': errors.has('enlaces_externos') }]">
 				<label for="enlaces_externos">Ruta del server</label>
-				<textarea class="form-control" rows="3" name="enlaces_externos"  id="enlaces_externos" v-model="tarea.enlaces_externos" placeholder="Ruta del server"></textarea>
+				<textarea class="form-control" rows="3" name="enlaces_externos"  id="enlaces_externos" v-model="tarea.enlaces_externos" placeholder="Ruta del server" v-validate data-vv-rules="required|min:4"></textarea>
+				<span  class="help-block" v-show="errors.has('enlaces_externos')">{{ errors.first('enlaces_externos') }}</span>
 			</div>
 			<div class="form-group required">
 				<label>Estado <sup>*</sup></label>
 				<select_estados tipo_estado="1"  :select="estado" ></select_estados>
 			</div>
 			<div class="form-group">
-				<label for="mapa_cliente">T. Mapa del cliente</label>
-				<input type="text" name="mapa_cliente" id="mapa_cliente"  class="form-control" >
+				<label for="tiempo_mapa_cliente">T. Mapa del cliente</label>
+				<input type="text" name="tiempo_mapa_cliente" id="tiempo_mapa_cliente" v-model="tarea.tiempo_mapa_cliente" class="form-control" placeholder="Tiempo Mapa del cliente">
 			</div>
 		</div>
 		<!-- /.box-body -->
@@ -103,11 +103,19 @@
 </template>
 <script>
 	import Datepicker from 'vuejs-datepicker';
+	import VeeValidate, { Validator } from 'vee-validate';
+
+  	Vue.use(VeeValidate);
 	module.exports = {
-		components: {Datepicker},
+		components: {Datepicker,VeeValidate,Validator},
 		data(){
 			return{
-				tarea:{},
+				tarea:{
+					nombre_tarea:'',
+					descripcion:'',
+					enlaces_externos:'',
+
+				},
 				ot:{},
 				area:{},
 				current_date:'',
@@ -116,13 +124,19 @@
 				fase:'',
 				user:'',
 				fecha_entrega_cliente:'',
+				message:'',
 				errors_return:{
 		          'nombre':'',
 		          'nit':'',
 		          'nombre_contacto':'',
 		          'telefono':'',
 		          'email':'',
-		        }
+		        },
+		        option_toast:{
+		          timeOut: 5000,
+		          "positionClass": "toast-top-center",
+		          "closeButton": true,
+		        },
 			}
 		},
 		created: function() {
@@ -145,9 +159,14 @@
 		computed:{},
 		watch: {},
 		methods:{
-			addUser:function(user) {
-
-			},
+			setErrors:function(object) {
+		        this.message='';
+		        var that = this;
+		        $.each(object, function(index, value) {
+		          that.message += '<strong>'+index + '</strong>: '+value+ '</br>';
+		          that.errors_return[index] = 'has-warning';
+		        });
+		    },
 			getCurrentDate:function(data='') {
 				var today;
 				if (!data) {
@@ -180,13 +199,34 @@
 				return today;
 			},
 			agregarTarea:function(e) {
-				this.tarea.fecha_entrega_cliente = this.getCurrentDate(this.fecha_entrega_cliente.toString());
+				this.$validator.validateAll();
+		        if (this.errors.any()) {
+		          return false
+		        }
+				this.tarea.fecha_entrega_cliente = (this.fecha_entrega_cliente)?this.getCurrentDate(this.fecha_entrega_cliente.toString()):null;
 				this.tarea.estados_id = this.estado.id;
 				this.tarea.ots_id= this.ot.id;
 				this.tarea.planeacion_fases_id = this.fase.id;
 				this.tarea.areas_id = this.area.id;
 				this.tarea.usuarios_id = this.user;
 				console.log(this.tarea);
+				this.$http.post('api/v1/tareas',this.tarea)
+		         .then(function(respuesta){
+		           if (respuesta.status != '200') {
+			            if (Object.keys(respuesta.body.obj).length>0) {
+			              this.setErrors(respuesta.body.obj);
+			            }
+			            toastr.warning(this.message,respuesta.body.msg,this.option_toast);
+			          } else {
+			            toastr.success(respuesta.body.msg,'',this.option_toast);
+			          }
+		           console.log(respuesta);
+		         }, (response) => {
+		          if (Object.keys(response.body.obj).length>0) {
+		            this.setErrors(response.body.obj);
+		          }
+		          toastr.error(this.message,response.body.msg,this.option_toast);
+		        });
 			},
 		},
 		mounted() {}
