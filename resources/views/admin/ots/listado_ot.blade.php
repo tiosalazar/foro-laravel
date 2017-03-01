06c4d4100c994fd5d50df9b0a329c4105cc18ceb
Alvaro Borrero <aborrero@himalayada.com>
2017-03-01 12:07:50 -0500

    titulo listado OT

--- resources/views/admin/ots/listado_ot.blade.php
+++ resources/views/admin/ots/listado_ot.blade.php
@@ -7,15 +7,15 @@
 
 @section('main-content')
 
-		<div class="container-fluid spark-screen">
+<div class="container-fluid spark-screen">
 	<div class="row">
+		<div class="titulos_foro">
+			<h3 class="box-title"><span class="span_descripcion1">Listado de</span> <span class="span_descripcion2"> OTs</span></h3>
+		</div>
 		<!-- col-md-8 -->
 		<div class="col-md-12 ">
 			<!-- box -->
 			<div class="box box-primary">
-				<div class="box-header with-border">
-					<h3 class="box-title">Listado de OTS</h3>
-				</div>
 				<div class="box-body  table-responsive">
 					<listar_ots></listar_ots>
 				</div>
