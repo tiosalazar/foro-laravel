3b7c7fe1e134d7cf0a9176b73926c6fecc1f3afe
Alvaro Borrero <aborrero@himalayada.com>
2017-04-18 14:46:03 -0500

    Editar y borrar cliente en permisos separados

--- database/seeds/ChangesAfterVersion_1_3.php
+++ database/seeds/ChangesAfterVersion_1_3.php
@@ -11,11 +11,17 @@ class ChangesAfterVersion_1_3 extends Seeder
      */
     public function run()
     {
-      $this->call('Crear_Permisos_Historico_Clientes');
-      $this->command->info('Se ha Creado el permiso Historico de clientes');
+      // $this->call('Crear_Permisos_Historico_Clientes');
+      // $this->command->info('Se ha Creado el permiso Historico de clientes');
+      //
+      // $this->call('Asignar_Permisos_Historico_Clientes');
+      // $this->command->info('Se ha Asignado  el permiso Historico de clientes');
 
-      $this->call('Asignar_Permisos_Historico_Clientes');
-      $this->command->info('Se ha Asignado  el permiso Historico de clientes');
+      $this->call('Crear_Permisos_Borrar_Cliente');
+      $this->command->info('Se ha Creado el permiso Borrar cliente');
+
+      $this->call('Asignar_Permisos_Borrar_Cliente');
+      $this->command->info('Se ha Asignado  el permiso Borrar cliente');
     }
 }
 class Crear_Permisos_Historico_Clientes extends Seeder {
@@ -35,6 +41,38 @@ class Asignar_Permisos_Historico_Clientes extends Seeder {
 
     //Permisos
     $permisos= App\Permission::where('name','ver_historico_clientes')->get();
+    //El Rol Desarrollo
+    $rol= App\Role::where('name','desarrollo')->first();
+    $rol->attachPermissions($permisos);
+
+   //El Rol owner
+    $rol= App\Role::where('name','owner')->first();
+    $rol->attachPermissions($permisos);
+
+    //El Rol Coordinador
+    //$rol= App\Role::where('name','coordinador')->first();
+    //$rol->attachPermissions($permisos);
+
+  }
+
+}
+class Crear_Permisos_Borrar_Cliente extends Seeder {
+
+  public function run()
+  {
+    //Permisos Sobre OTS
+  App\Permission::create(array('name' => 'borrar_clientes','display_name'=>'Borrar cliente','description'=>'Puede borrar un cliente' ));
+
+  }
+
+}
+class Asignar_Permisos_Borrar_Cliente extends Seeder {
+
+  public function run()
+  {
+
+    //Permisos
+    $permisos= App\Permission::where('name','borrar_clientes')->get();
     //El Rol Desarrollo
     $rol= App\Role::where('name','desarrollo')->first();
     $rol->attachPermissions($permisos);
