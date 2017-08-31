b554b1498be6707ac3792492870796f981d1a21c
Alvaro Borrero <aborrero@himalayada.com>
2017-08-31 14:41:52 -0500

    Ajuste error ver solicitud vue js y agregado al sidebar link de de requerimiento

--- resources/assets/js/components/clientes/ver_solicitud.vue
+++ resources/assets/js/components/clientes/ver_solicitud.vue
@@ -41,8 +41,9 @@
 <script>
 
 module.exports = {
-data(){
-	 props: ['requerimientoarray'],
+	props: ['requerimientoarray'],
+    data(){
+	
 	return{
 		comentarios_array:[],
 		info_requerimiento:[],
--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -57,7 +57,7 @@
        <li class="treeview">
         <a href="#"><i class="fa fa-ticket" aria-hidden="true"></i> <span>{{ trans('texto_menu.requerimientos_cliente') }}</span> <i class="fa fa-caret-left  fa-lg pull-right"></i></a>
         <ul class="treeview-menu">
-          <li><a href="">{{  trans('texto_menu.crear_requerimientos_cliente') }}</a></li>
+          <li><a href="{{url('/crear_requerimiento')}}">{{  trans('texto_menu.crear_requerimientos_cliente') }}</a></li>
           <li><a href="">{{  trans('texto_menu.listar_requerimientos_cliente') }}</a></li>
         </ul>
       </li>
