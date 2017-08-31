7265bfa8e3a128b0d14e841cc697d47e087fdebf
dsalazar <dsalazar@himalayada.com>
2017-08-31 08:54:31 -0500

    Arreglando el repositorio, se removio la carpeta resources que estaba dentro de app

--- app/resources/views/vendor/adminlte/layouts/partials/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Main Footer -->
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-        <!--<a href="https://github.com/acacha/adminlte-laravel"></a><b>admin-lte-laravel</b></a>. {{ trans('adminlte_lang::message.descriptionpackage') }}-->
-    </div>
-    <!-- Default to the left -->
-   Copyright &copy; 2017  <strong> <a target="_blank" href="http://himalayadigitalagency.com/">Himalaya Digital Agency</a></strong>.  Todos los derechos reservados. <!--{{ trans('adminlte_lang::message.createdby') }} <a href="http://acacha.org/sergitur">Sergi Tur Badenas</a>. {{ trans('adminlte_lang::message.seecode') }} <a href="https://github.com/acacha/adminlte-laravel">Github</a>-->
-</footer>
